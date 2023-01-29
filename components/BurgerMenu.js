4be5401bef40d4f91c408a03dec47f29c7ca9d5b
tacowasabii <godseoril812@gmail.com>
2023-01-29 18:26:56 +0900

    burger menu

---
+++ components/BurgerMenu.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+const BurgerMenu = ({ setActive, active }) => {
+  return (
+    <div
+      className="block md:hidden cursor-pointer z-50"
+      data-drawer-target="drawer-example"
+      data-drawer-show="drawer-example"
+      aria-controls="drawer-example"
+      onClick={() => setActive(!active)}
+    >
+      <Image
+        src="public/menu-icon.svg"
+        alt="menu icone"
+        width={22}
+        height={15}
+      />
+    </div>
+  );
+};
+
+export default BurgerMenu;
