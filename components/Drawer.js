22b9f3d90cf34ec09abf8d2cc5864351ee696920
tacowasabii <godseoril812@gmail.com>
2023-02-02 16:19:06 +0900

    drawer

--- components/Drawer.js
+++ components/Drawer.js
@@ -1,5 +1,5 @@
+import Image from "next/image";
 import { useEffect } from "react";
-import { Image } from "next/image";
 import Link from "next/link";
 
 const Drawer = ({ setActive, active }) => {
