3f7b61b979c1fdff992b88be8bd1379c4c1fea9b
tacowasabii <godseoril812@gmail.com>
2023-02-02 16:21:09 +0900

    bestsellersdata

--- data/bestSellersData.js
+++ data/bestSellersData.js
@@ -1,31 +1,31 @@
 const bestSellersData = [
   {
-    image: '/content-img-1.png',
-    title: 'Candy',
-    price: '$23.25',
+    image: "/content-img-1.png",
+    title: "Candy",
+    price: "$23.25",
     btnLayout: {
-      textColor: 'text-black',
-      bgColor: 'bg-orange-300',
+      textColor: "text-black",
+      bgColor: "bg-orange-300",
     },
   },
   {
-    image: '/content-img-2.png',
-    title: 'Lollipop',
-    price: '$15.33',
+    image: "/content-img-2.png",
+    title: "Lollipop",
+    price: "$15.33",
     btnLayout: {
-      textColor: 'text-white',
-      bgColor: 'bg-pink-400',
+      textColor: "text-white",
+      bgColor: "bg-pink-400",
     },
   },
   {
-    image: '/gum.png',
-    title: 'Gum',
-    price: '$23.25',
+    image: "/gum.png",
+    title: "Gum",
+    price: "$23.25",
     btnLayout: {
-      textColor: 'text-white',
-      bgColor: 'bg-pink-400',
+      textColor: "text-white",
+      bgColor: "bg-pink-400",
     },
   },
-]
+];
 
-export default bestSellersData
+export default bestSellersData;
