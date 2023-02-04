0cd7f4fb6432d24ef0c589b8afd79ca8badb1dd0
tacowasabii <godseoril812@gmail.com>
2023-02-04 16:03:19 +0900

    subtotal

--- components/cart/Subtotal.js
+++ components/cart/Subtotal.js
@@ -1,9 +1,9 @@
 const Subtotal = () => {
   return (
-    <div className="border-2 rounded-2xl border-slate-400 bg-white p-5 mb-3">
+    <div className="border-2 rounded-3xl border-custom-gray bg-white px-5 py-5">
       <div className="flex justify-between mb-4">
         <div className="font-poppins text-sm text-custom-purple">Subtotal:</div>
-        <div className="font-poppins text-sm text-custom-red-2">$32.68</div>
+        <div className="font-poppins text-base text-custom-red-2">$32.68</div>
       </div>
       <div className="flex justify-between mb-4">
         <div className="font-poppins text-sm text-custom-purple">
@@ -12,12 +12,14 @@ const Subtotal = () => {
         <div className="font-poppins text-sm text-custom-red-2">$5.00</div>
       </div>
       <div className="flex justify-between mb-4">
-        <div className="font-poppins text-sm text-custom-purple">
+        <div className="font-poppins text-sm text-custom-purple font-bold">
           Estimated total:
         </div>
-        <div className="font-poppins text-sm text-custom-red-2">$36.68</div>
+        <div className="font-poppins text-2xl font-bold text-custom-red-2">
+          $36.68
+        </div>
       </div>
-      <div className="flex justify-between ">
+      <div className="flex justify-between">
         <button
           type="button"
           className="bg-custom-pink font-poppinsbold w-full rounded-full p-5 text-white text-center"
