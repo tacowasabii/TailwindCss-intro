f52394bf20ec1c302216a252e93e0e000c4d3c17
tacowasabii <godseoril812@gmail.com>
2023-02-04 16:03:12 +0900

    promocode

--- components/cart/Promocode.js
+++ components/cart/Promocode.js
@@ -1,6 +1,6 @@
 const Promocode = () => {
   return (
-    <div className="border-2 rounded-2xl border-slate-400 bg-white p-5 mb-3">
+    <div className="border-2 rounded-3xl border-custom-gray bg-white p-5 mb-3">
       <div className="font-fredoka font-semibold font-24 text-custom-purple">
         Promocode
       </div>
@@ -12,14 +12,14 @@ const Promocode = () => {
               className="bg-custom h-10 w-full border-0 px-4 rounded-full focus:outline-none hover:cursor-pointer"
             />
             <button className="rounded-full bg-orange-400 absolute top-0 right-0 text-sm h-10 px-3 text-black font-bold">
-              OK
+              Ok
             </button>
           </div>
         </div>
       </div>
-      <div className="font-poppins text-custom-purple text-sm mt-2">
-        <span className="text-custom-red-2">Sign In</span>{" "}
-        <span>to see all rewards</span>
+      <div className="font-poppins text-custom-purple text-xs mt-2">
+        <span className="text-custom-red-2 underline">Sign In</span>{" "}
+        <span className="text-custom-purple">to see all rewards</span> .
       </div>
     </div>
   );
